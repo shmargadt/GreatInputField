bf6cf95b659d7bebffe6cd35a64c1961b0e7c6da
Tomer Shmargad <tomer@spectory.com>
2016-12-06 07:29:11 +0200

    react-input-config

---
+++ ReactInputField/my-app/src/DumpInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DumpInput = (props) => {
+  return (
+
+      {props}
+
+
+  );
+};
+
+
+export default DumpInput;
---
+++ ReactInputField/my-app/src/GreatInput.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import DumpInput from './DumpInput';
+
+class GreatInput extends Component {
+  render() {
+    return (
+      <div className="great-input-container">
+        <input ></input>
+      </div>
+    );
+  }
+}
+
+export default GreatInput;
