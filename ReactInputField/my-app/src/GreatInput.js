74f3b1f0ec62d26082a20c14d8d7146bd94492f2
Tomer Shmargad <tomer@spectory.com>
2017-01-02 16:57:34 +0200

    css react

--- ReactInputField/my-app/src/GreatInput.js
+++ ReactInputField/my-app/src/GreatInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './index.css';
 import DumpInput from './DumpInput';
 
 class GreatInput extends Component {
@@ -7,7 +7,7 @@ class GreatInput extends Component {
     super(props);
     this.state = {
       input: '',
-      new_input: '123'
+      new_input: ''
     }
     this.onRenderNewInput = this.onRenderNewInput.bind(this);
     this.onChangeInput = this.onChangeInput.bind(this);
@@ -15,14 +15,15 @@ class GreatInput extends Component {
   }
 
    onRenderNewInput = function () {
-    this.setState({new_input: this.changeAsteriskToBold(this.state.input)});
+    const result_title = "<div class='result-title'> Result:</div>";
+    if (!this.state.input) {return;}
+    this.setState({new_input: result_title + this.changeAsteriskToBold(this.state.input)});
     console.log(this.state.new_input);
   }
 
    onChangeInput = function (e) {
     this.state.input = e.target.value;
   }
-
 
   changeAsteriskToBold = function (text) {
     if (!text) { return; }
@@ -50,10 +51,11 @@ class GreatInput extends Component {
   
   render() {
     return (
-      <div className="great-input-container">
-        <input onChange={this.onChangeInput} id="" />
-        <div onClick={this.onRenderNewInput}><b> Click to ender new input</b></div>
-        <div> New Input: </div>
+      <div >
+        <div class="title"> MY GREAT INPUT </div>
+        <input onChange={this.onChangeInput} id="" class="great-input" />
+        <div class="instr"> *bold* _italics_  >qoute </div>
+        <div onClick={this.onRenderNewInput} class="convert-btn">Convert text</div>
         <div dangerouslySetInnerHTML={{__html: this.state.new_input}}></div>
       </div>
     );
